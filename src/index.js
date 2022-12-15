c7abe351b1d3a8cd5dfbf8711c901a2f696b5182
Mayank Pathak <65023603+mayankpathak10299@users.noreply.github.com>
2022-12-15 12:59:36 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ console.log(fun[0]);
 ReactDOM.render(
   <>
   <div>
-      <h1  className="heading">Yoga Admission Form</h1>
+      <h1  className="heading">Yoga Registration Form</h1>
       <div className="gallery">
       <img src='https://img.freepik.com/free-vector/silhouette-female-yoga-pose-against-mandala-design_1048-13082.jpg?w=740&t=st=1670653697~exp=1670654297~hmac=e155f54bbcb9f0bfcac89acbc22d4a310f90041b6daeec128e010074a26c31a1' alt='logo'/>  
       </div>   
